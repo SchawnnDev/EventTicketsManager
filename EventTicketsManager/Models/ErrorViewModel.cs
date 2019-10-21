ab358fb70109206af3cbf3303d73179a0b8cfd03
SchawnnDev <iperskill@gmail.com>
2019-10-21 10:53:23 +0200

    init repo

---
+++ EventTicketsManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EventTicketsManager.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
